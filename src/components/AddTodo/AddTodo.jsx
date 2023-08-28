3d80dc9028bf2269abcbedb9af8a572bd29609c3
Odaeun <qzsz797979@gmail.com>
2023-08-28 09:26:56 +0900

    추가수정

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import styles from './AddTodo.module.css'
+
+export default function AddTodo({onAdd}) {
+  const [text, setText] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(text.trim().length === 0) {
+      setText('');
+      return;
+    }
+    onAdd({id: uuidv4(), text, status: 'active'});
+    setText('');
+  }
+  const handleChange = (e) => setText(e.target.value);
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <input type='text' className={styles.input} placeholder='Add Todo' value={text} onChange={handleChange} />
+      <button className={styles.button}>Add</button>
+    </form>
+  );
+}
+
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import styles from './Todo.module.css';
+
+export default function Todo({todo, onUpdate, onDelete}) {
+  const {text, status} = todo;
+  const handleChange = (e) => {
+    const status = e.target.checked ? 'completed' : 'active';
+    onUpdate({...todo, status})
+  }
+  const handleDeleted = () => onDelete(todo);
+  return (
+    <li className={styles.todo}>
+      <input type='checkbox' id={todo.id} className={styles.checkbox} checked={status === 'completed'} onChange={handleChange} />
+      <label htmlFor={todo.id} className={styles.text}>{text}</label>
+      <span className={styles.icon}>
+        <button type='button' className={styles.button} onClick={handleDeleted}><FaTrashAlt /></button>
+      </span>
+    </li>
+  );
+}
+
